0c3604e23b03d29f90f372133a1ed6585017fdbd
Sarfaraz Shaikh <39793740+sarfarazgit@users.noreply.github.com>
2024-11-05 18:59:06 +0530

    Add files via upload

---
+++ main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+func main() {
+
+	fmt.Println("\n ***  Welcome To The Stock Exchange Marcket ***")
+
+	// Preparing the Stock data
+
+	stockdata := Stock{"SBI stock", 150}
+
+	// Sending for conversion (Marshaling)
+	var jsondata = StructureTojson(stockdata)
+
+	fmt.Printf("Your Stock details in jsondata: %v\n", jsondata)
+
+	// Sending for conversion (Unmarshalling)
+	JsonTostructure([]byte(jsondata))
+}
+
+// Converting structure data to json formate (Marshaling)
+
+func StructureTojson(stockdata Stock) string {
+
+	var jsonresult, err = json.Marshal(stockdata)
+
+	if err != nil {
+
+		return ("Invalid data recieved for conversion:")
+	}
+
+	return string(jsonresult)
+}
+
+// Converting JSON data into Structure formate (Unmarshaling)
+
+func JsonTostructure(Jsondata []byte) {
+
+	var stockdata Stock
+	er := json.Unmarshal(Jsondata, &stockdata)
+
+	if er != nil {
+
+		fmt.Println("Invalid data recieved for conversion:", er)
+		return
+	}
+
+	fmt.Println("Your stock data post conversion:", stockdata)
+}
+
+type Stock struct {
+	Stockname  string
+	TotalStock int
+}
