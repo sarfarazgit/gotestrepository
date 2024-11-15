4808374e11801780ec4ce49b51f063dbdcc668c2
Sarfaraz Shaikh <39793740+sarfarazgit@users.noreply.github.com>
2024-11-15 14:53:00 +0000

    new code adding in folder

--- genericsgo/Genericsmain.go
+++ genericsgo/Genericsmain.go
@@ -2,27 +2,35 @@ package main
 
 import "fmt"
 
+// It supports to (int,string and boolean) type of data:
+
 func displayElements[T int | string | bool](a, b T) {
-	fmt.Println(a, b)
+	fmt.Println("\t displayElements() data \n\t", a, b)
 }
 
+// It supports to (any : Whatever it contains the types) type of data:
 func arrayElements[T any](elements []T) {
 
-	for val := range elements {
-		fmt.Println(val)
+	fmt.Println("\n\t arrayElements() data:\n")
+
+	for _, value := range elements {
+		fmt.Print("\t", value, "\n")
 	}
 
 }
 
+// To manage Generics by using predefined comparable
 func mapElements[T comparable](mapelements map[T]T) {
+	fmt.Println("\n\t Map Elements:\n")
+
 	for key, val := range mapelements {
-		fmt.Println(key, val)
+		fmt.Println("\t", key, ":", val, ",")
 	}
 }
 
 func main() {
 
-	displayElements(100, 200)
+	displayElements(false, true)
 
 	// Data preparation for array
 	elements := []string{"Hello", "Hi", "Bye"}
